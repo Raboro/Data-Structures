6f82d21d9a3db3308457ef104b519cc81173618a
Raboro <raborogit@gmail.com>
2022-06-21 00:01:37 +0200

    finished stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -5,12 +5,12 @@ from dataclasses import dataclass, field
 class Stack:
     stack: list = field(default_factory=list)
 
-    def add(self, new_element: type):
+    def add(self, new_element: type) -> None:
         self.stack.append(new_element)
 
-    def pop(self):
+    def pop(self) -> None:
         self.stack.remove(self.stack[-1])
 
-    def print(self):
+    def print(self) -> None:
         for element in self.stack:
             print(element)
